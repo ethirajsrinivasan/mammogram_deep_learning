dc6598db067715e804d908b295ac4a06cc7e58fe
Ethiraj Srinivasan <e0146968@nus.edu.sg>
2018-01-31 00:59:45 +0800

    update script

--- contour_detection/preprocess_image.py
+++ contour_detection/preprocess_image.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import os
-os.chdir("/home/ethi/ethi/apps/advance_research/all-mias-png/")
-images = glob.glob("*.png")
+os.chdir('C:/Users/E0146968/Downloads/standardisedImages/')
+images = glob.glob("*.jpg")
 for im in images:
     print(im)
     image = cv2.imread(im)
@@ -13,13 +13,14 @@ for im in images:
     ret,thresh = cv2.threshold(gray,10,255,0)
     _, contours, _ = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
     contourAreas = [cv2.contourArea(contour) for contour in contours ]
-    largest_contour = contourAreas.index(max(contourAreas))
-    mask = np.ones(image.shape[:2], dtype="uint8") * 255
-    # loop over the contours
-    for i,value in enumerate(contours):
-        # if the contour is bad, draw it on the mask
-        if i ==largest_contour:
-            cv2.drawContours(mask, [value], -1, 0, -1)
-    mask_inv = cv2.bitwise_not(mask)
-    image = cv2.bitwise_and(image, image, mask=mask_inv)
-    cv2.imwrite('../preprocess/'+im,image)
+    if len(contourAreas) > 0:
+        largest_contour = contourAreas.index(max(contourAreas))
+        mask = np.ones(image.shape[:2], dtype="uint8") * 255
+        # loop over the contours
+        for i,value in enumerate(contours):
+            # if the contour is bad, draw it on the mask
+            if i ==largest_contour:
+                cv2.drawContours(mask, [value], -1, 0, -1)
+        mask_inv = cv2.bitwise_not(mask)
+        image = cv2.bitwise_and(image, image, mask=mask_inv)
+        cv2.imwrite('../preprocess/'+im,image)
